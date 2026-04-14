046bb15c6345a88117e0455959f3ed8d1b55027e
Pandari1 <166743165+Pandari1@users.noreply.github.com>
2026-04-15 00:57:33 +0530

    ok

--- environments/production/outputs.tf
+++ environments/production/outputs.tf
@@ -21,19 +21,3 @@ output "pods_range_name" {
 output "services_range_name" {
   value = module.networking.services_range_name
 }
-
-output "terraform_state_bucket_name" {
-  value = module.bootstrap.terraform_state_bucket_name
-}
-
-output "core_service_account_email" {
-  value = module.bootstrap.core_service_account_email
-}
-
-output "compute_service_account_email" {
-  value = module.bootstrap.compute_service_account_email
-}
-
-output "workload_identity_provider_name" {
-  value = module.bootstrap.workload_identity_provider_name
-}
